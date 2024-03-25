d6f167dad375ac718a4b7e0a5616f8028a756f59
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-03-25 16:10:52 +0100

    feat: add create table migration

--- database/migrations/2024_03_25_145234_create_comics_table.php
+++ database/migrations/2024_03_25_145234_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price', 20);
+            $table->string('serie', 30);
+            $table->date('sale_date');
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
